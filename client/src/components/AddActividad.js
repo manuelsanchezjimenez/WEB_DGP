bf478d8e21cb64858d7f320d52db4529c74d47f9
Lehara20 <74658801+Lehara20@users.noreply.github.com>
2022-10-31 16:05:52 +0100

    Casi terminado (falta meter images en BD)

--- client/src/components/AddActividad.js
+++ client/src/components/AddActividad.js
@@ -1,5 +1,6 @@
 import axios from "axios"
 import React, { Component } from "react"
+import ReactDOM from 'react-dom';
 import { Link } from 'react-router-dom'
 import { useState } from 'react';
 import Header from "./Header"
@@ -16,49 +17,153 @@ export default class AddActividad extends Component {
          // alumnos: [],
          newNameAct: '',
          newDesrAct: '',
+         image: [],
+         totalImages: 0,
       };
+      this.renderCrudDiv = this.renderCrudDiv.bind(this);
+      this.listaImages = this.listaImages.bind(this);
+
       this.handleChange = this.handleChange.bind(this);
-      this.handleSubmit = this.handleSubmit.bind(this);
+      this.handleSubmitData = this.handleSubmitData.bind(this);
+      this.handleSubmitImage = this.handleSubmitImage.bind(this);
+      this.deleteImage = this.deleteImage.bind(this);
    }
 
    handleChange(event) {
-      // this.setState({ value: event.target.value });
       this.setState({ [event.target.name]: event.target.value.trim() });
    }
 
-   handleSubmit(event) {
+   validate() {
+      if (this.state.newNameAct == '' || this.state.newDesrAct == '') {
+         alert(' Los campos de nombre y descripción no pueden estar vacíos');
+         return false;
+      }
+      return true;
+   }
 
-      alert('Nombre: "' + this.state.newNameAct + '"\nDescripción: "' + this.state.newDesrAct+'"');
-      var bodyFormData = new FormData();
-      bodyFormData.append('nombre', this.state.newNameAct)
-      bodyFormData.append('descripcion', this.state.newDesrAct)      
-      
-      axios({
-         method: "post",
-         url: `${SERVER_HOST}/actividades/add`,
-         data: bodyFormData,
-         headers: { "Content-Type": "multipart/form-data" },
-     }).then(res => {
-         //handle success
-      alert(' ¡Hecho! ');
-      // location.replace('localhost:3000/ListaActividades');
+   handleSubmitData(event) {
+      if (this.validate()) {
+         alert('Nombre: "' + this.state.newNameAct + '"\nDescripción: "' + this.state.newDesrAct + '"');
+         var bodyFormData = new FormData();
+         bodyFormData.append('nombre', this.state.newNameAct)
+         bodyFormData.append('descripcion', this.state.newDesrAct)
+         axios({
+            method: "post",
+            url: `${SERVER_HOST}/actividades/add`,
+            data: bodyFormData,
+            headers: { "Content-Type": "multipart/form-data" },
+         }).then(res => {
+            //handle success
+            alert('Actividad añadida, se van a guardar las imágenes...');
+            this.handleSubmitImage(event);
+         }).catch(err => {
+            //handle error
+            alert('No se ha podido guardar la actividad');
+            this.setState({ logInError: true, errorMessage: 'Error, no se ha podido guardar la actividad.' })
+         });
+         event.preventDefault();
+      }
+   }
 
-         // localStorage.usuario = res.data.usuario
-         // localStorage.accessLevel = res.data.accessLevel
-         // localStorage.token = res.data.token
+   handleSubmitImage(event) {
+      for (let i = 0; i < totalImages; i++) {
+         alert('Guardando imagen: "' + this.state.image[i].value + '"\nDescripción: "' + this.state.newDesrAct + '"');
+         var bodyFormData = new FormData();
+         bodyFormData.append('nombre', i)
+         bodyFormData.append('imagen', this.state.image[i])
+         axios({
+            method: "post",
+            url: `${SERVER_HOST}/actividades/add`,
+            data: bodyFormData,
+            headers: { "Content-Type": "multipart/form-data" },
+         }).then(res => {
+            //handle success
+            alert('Actividad añadida, se van a guardar las imágenes...');
+            this.handleSubmitImage(event);
+         }).catch(err => {
+            //handle error
+            alert('No se ha podido guardar la actividad');
+            this.setState({ logInError: true, errorMessage: 'Error, no se ha podido guardar la actividad.' })
+         });
+         event.preventDefault();
+      }
+   }
 
-         // if(res.data.accessLevel === ACCESS_LEVEL_ADMIN){
-         //     this.setState({redirectAdmin:true})
-         // }else{
-         //     this.setState({redirectTeacher:true})
-         // }
-     }).catch(err => {
-         //handle error
-      alert(' Vaya, parece que algo ha ido mal ');
-         this.setState({logInError: true, errorMessage: 'Error, no se ha podido guardar la actividad.'})
-     });
+   handleFileChange = (event) => {
+      if (event.target.files) {
+         let reader = new FileReader();
+         reader.onload = (e) => {
+            // this.setState({ image: e.target.result });
+            this.state.image[this.state.totalImages] = e.target.result;
+            this.renderCrudDiv();
+            // this.setState({ totalImages: totalImages + 1 });
+            // this.state.totalImages += 1;
+         };
+         reader.readAsDataURL(event.target.files[0]);
+         event.target.value = null;
+      }
+   }
 
-      event.preventDefault();
+   renderCrudDiv() {
+      this.setState({ totalImages: this.state.totalImages + 1 });
+   }
+
+   listaImages() {
+      const pictos = [];
+      for (let i = 0; i < this.state.totalImages; i++) {
+         pictos.push(
+            <div className="contenedorPicto objectLine" key="{i}" >
+               {i + 1}
+               <div className="contImage ">
+                  <img className="imagen" id="target" src={this.state.image[i]} />
+               </div>
+               <span>
+                  <button className="boton b1" onClick={() => { this.moveImage(i, (i - 1 + this.state.totalImages) % this.state.totalImages) }}>
+                     <span>&#8592;</span>
+                  </button>
+                  <button className="boton b1" onClick={() => { this.deleteImage(i) }}>
+                     <span>&#128465;</span>
+
+                  </button>
+                  <button className="boton b1" onClick={() => { this.moveImage(i, (i + 1) % this.state.totalImages) }}>
+                     <span>&#8594;</span>
+                  </button>
+
+                  {/* {this.generateButton(i,this.state.totalImages)} */}
+               </span>
+            </div>
+         );
+      }
+      // this.state.insertImageMode = false;
+      return pictos;
+   }
+
+   moveImage(item, move) {
+      alert('Se quiere mover la imagen ' + item + ' en la posición ' + move)
+      // Metodo 1: No se debe modificar directamente this.state pero slice es un método poco fiable (pueden mutar los datos)
+      let k, copyImage = this.state.image.slice();
+      k = copyImage[item];
+      copyImage[item] = copyImage[move]
+      copyImage[move] = k;
+      this.setState({ image: copyImage });
+      // Metodo 2: Se modifica directamente this.state pero los datos se modifican de manera más segura
+      // let k;
+      // k = this.state.image[item];
+      // this.state.image[item] = this.state.image[move]
+      // this.state.image[move] = k;
+      // this.setState({ totalImages: this.state.totalImages  });
+   }
+
+   deleteImage(item) {
+      alert('Se quiere eliminar la imagen ' + item)
+      // Metodo 1: No se debe modificar directamente this.state pero slice es un método poco fiable (pueden mutar los datos)
+      this.setState({ totalImages: this.state.totalImages-1 });
+      let copyImage = this.state.image.slice();
+      copyImage.splice(item, 1);
+      this.setState({ image: copyImage });
+      // this.setState({ totalImages: this.state.totalImages-1 });
+      // Metodo 2: Se modifica directamente this.state pero los datos se modifican de manera más segura
+      // this.state.image.splice(item, 1);
    }
 
    render() {
@@ -67,7 +172,7 @@ export default class AddActividad extends Component {
             <Header />
             Añadir nueva actividad
             <div className="formulario">
-               <form onSubmit={this.handleSubmit}>
+               <form onSubmit={this.handleSubmitData}>
                   <div>
                      <label>Nombre de la tarea:</label><br />
                      <input type="text" id="newNameAct" name="newNameAct" placeholder="Nombre de la nueva actividad" onChange={this.handleChange} />
@@ -76,13 +181,31 @@ export default class AddActividad extends Component {
                      <label>Descripción:</label><br />
                      <textarea id="newDesrAct" name="newDesrAct" placeholder="Descripción de la nueva actividad" onChange={this.handleChange} />
                   </div>
-                  <div>
-                     Secuencia de pictogramas
+
+               </form>
+               <div>
+                  Secuencia de pictogramas
+                  <div id="ContenedorImagenes">
+                     {/* <img id="target" src={this.state.image[]} /> */}
                   </div>
-                  <button className="boton b1" type="submit">
+
+                  <input type="file" onChange={this.handleFileChange} />
+
+
+
+                  <div className="crud-container">
+                     <div className="btnContainer" >
+                        {/* <button className="makeDivsBtn" onClick={this.renderCrudDiv}>
+                              Create Divs
+                           </button> */}
+                     </div>
+                     {this.listaImages()}
+                  </div>
+                  <button className="boton b1" onClick={this.handleSubmitData}>
                      Añadir
                   </button>
-               </form>
+
+               </div>
             </div>
          </div>
       );
