f09528c396a6c790fecec016d55cd21551bef0c1
Lehara20 <74658801+Lehara20@users.noreply.github.com>
2022-11-05 18:05:54 +0100

    Update actividades.js

--- server/routes/actividades.js
+++ server/routes/actividades.js
@@ -18,50 +18,30 @@ var storage = multer.diskStorage({
         cb(null, file.fieldname + '-' + Date.now())
     }
 });
-  
 var upload = multer({ storage: storage });
 
-const findActividad = (req, res, next) =>
-{
-    actividadModel.findOne({nombre: req.body.nombre}, (error, data) =>{
-        if(error){
-            console.log(error)
-        }else{
-            if(data){
-                req.user = data
-                return next()
-            }else
-                return next(createError(400, "Actividad not found."))
-        } 
-    })
-}
-
-const addProperty = (req, res, next) =>
-{
-    let property = new Object()
-    property.tenant = req.body.tenant
-    property.address = req.body.address
-    property.area = req.body.area
-    property.price = req.body.price
-    property.residents = []
-    property.images = []
-
-    req.files.map((file, index) =>
-    {
-        property.images[index] = {filename:`${file.filename}`}
-    })
-
-    propertiesModel.create(property, (error, data) =>
-    {
-        if(error){
-            return next(createError(400, `Error on property creation.`))
-        }else{
-            res.json(data)
-        }
-        
-    })
-}
-
+// const addProperty = (req, res, next) =>
+// {
+//     let property = new Object()
+//     property.tenant = req.body.tenant
+//     property.address = req.body.address
+//     property.area = req.body.area
+//     property.price = req.body.price
+//     property.residents = []
+//     property.images = []
+//     req.files.map((file, index) =>
+//     {
+//         property.images[index] = {filename:`${file.filename}`}
+//     })
+//     propertiesModel.create(property, (error, data) =>
+//     {
+//         if(error){
+//             return next(createError(400, `Error on property creation.`))
+//         }else{
+//             res.json(data)
+//         }
+//     })
+// }
 
 const addActividad = (req, res, next) => 
 {
@@ -69,11 +49,9 @@ const addActividad = (req, res, next) =>
         //     nombre:req.body.nombre,
         //     descripcion:req.body.descripcion,
         // }
-
     let actividad = new Object()
     actividad.nombre = req.body.nombre
     actividad.descripcion = req.body.descripcion
-
     actividadModel.create(actividad, (error, data) =>
     {
         if(error){
@@ -86,8 +64,40 @@ const addActividad = (req, res, next) =>
     })
 }
 
-// router.get(`/actividades/find`, findActividad)
+const getActividades = (req, res, next) => 
+{
+    actividadModel.find({}, (error, data) =>{
+        if(error){
+            console.log(error)
+        }else{
+            if(data){
+                res.json(data);
+                return res;
+            }else
+                return next(createError(400, "Activities not found."))
+        } 
+    })
+
+}
+
+const findActividad = (req, res, next) =>
+{
+    actividadModel.findOne({nombre: req.body.nombre}, (error, data) =>{
+        if(error){
+            console.log(error)
+        }else{
+            if(data){
+                req.user = data
+                return next()
+            }else
+                return next(createError(400, "Activity not found."))
+        } 
+    })
+}
+
 router.post(`/actividades/add`, upload.none(), addActividad)
+router.get(`/actividades/getAll`, getActividades)
+router.get(`/actividades/:nombre`, findActividad)
 
 
 module.exports = router
