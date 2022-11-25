a4e2a020e53d14a99081daad077ceb6ff243f18b
Lehara20 <74658801+Lehara20@users.noreply.github.com>
2022-11-25 09:25:59 +0100

    Update actividad_imagen.js

--- server/models/actividad_imagen.js
+++ server/models/actividad_imagen.js
@@ -1,12 +1,14 @@
 const mongoose = require(`mongoose`)
 
 let actividad_imagenSchema = new mongoose.Schema(
-   {
-     nombreAct: {type: String, required: true, unique: true},
-     imagen: {type: String, required: true}
-   },
-   {
-        collection: `actividad_imagen`
-   })
+  {
+    actividad: { type: String, required: true, unique: true },
+    orden: { type: Number, required: true, unique: true },
+    imagen: { type: String, required: true }
+  },
+  {
+    collection: `actividad_imagen`
+  })
 
+actividad_imagenSchema.index({ actividad: 1, orden: 1 }, { unique: true })
 module.exports = mongoose.model(`actividad_imagen`, actividad_imagenSchema)
