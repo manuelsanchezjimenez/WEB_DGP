20534e7113fb6841a9f3fcafe92a2f23ce65440f
Andrés Piqueras Brück <57135913+AndresPiquerasBruck@users.noreply.github.com>
2022-11-29 17:55:13 +0100

    ListaTareas.js actualizado para que el css funcionce

--- client/src/components/ListaTareas.js
+++ client/src/components/ListaTareas.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom'
 import Header from "./Header"
 import { SERVER_HOST } from "../config/global_constants"
 import "../css/AdminAlumPrincipal.css"
+import "../css/ListaTareas.css"
 
 const TarRow = ({ nombre, fechaInicio, fechaFinal, completado, alumno, type }) => {
    var tipo;
@@ -141,17 +142,19 @@ export default class ListaTareas extends Component {
       let i = 0;
       unaTarea.push(
          <div key={i++}>
-            <input label="Search" onChange={this.onChange} placeholder="Buscar Tarea..." />
+            <div className="buscarTarea">
+               <input label="Search" onChange={this.onChange} placeholder="Buscar Tarea..." />
+            </div>
             <div>
                <table className="table table-bordered" >
-                  <tbody>
+                  <tbody className="tablitaRellenita">
                      <tr>
                         <th
                            style={{ cursor: "pointer" }}
                            // onClick={() => { this.sortResults() }}
                            id="name"
                            onClick={() => { this.sorting("alumno") }}>
-                           Alumno {!this.state.order.alumno ? null : this.state.order.alumno === "asc" ? <span>&#9650;</span> : <span>&#9660;</span>}
+                           Alumno           {!this.state.order.alumno ? null : this.state.order.alumno === "asc" ? <span>&#9650;</span> : <span>&#9660;</span>}
                         </th>
                         <th
                            style={{ cursor: "pointer" }}
@@ -172,7 +175,7 @@ export default class ListaTareas extends Component {
                            // onClick={() => { this.sortResults() }}
                            id="name"
                            onClick={() => this.sorting("fechaFinal")}>
-                           Fecha final {!this.state.order.fechaFinal ? null : this.state.order.fechaFinal === "asc" ? <span>&#9650;</span> : <span>&#9660;</span>}
+                           Fecha de finalización {!this.state.order.fechaFinal ? null : this.state.order.fechaFinal === "asc" ? <span>&#9650;</span> : <span>&#9660;</span>}
                         </th>
                         <th
                            style={{ cursor: "pointer" }}
@@ -210,6 +213,7 @@ export default class ListaTareas extends Component {
       );
       return unaTarea;
    }
+   
 
 
    render() {
@@ -219,18 +223,21 @@ export default class ListaTareas extends Component {
             <h1>Lista de tareas</h1>
             {this.state.error ? <div>Error: {this.state.error.message}</div> : null}
             {this.state.mounted ? null : <div> Cargando tareas... </div>}
-            {this.showTable()}
-            <div className="Body">
-               <div className="botonesContainer">
-                  <Link to="/AddTareaAct"><input id="addTareaAct" type="button" value="ASIGNAR ACTIVIDAD" /></Link>
-                  <Link to="/ListaActividades"><input id="toggleTareas" type="button" value="VER POOL DE ACTIVIDADES" /></Link>
+            <div className="left">
+               {this.showTable()}
+            </div>
+            <div className="right">
+               <div className="Body">
+                  <div className="botonesContainer">
+                     <Link to="/AddTareaAct"><input id="addTareaAct" type="button" value="ASIGNAR ACTIVIDAD" /></Link>
+                     <Link to="/ListaActividades"><input id="toggleTareas" type="button" value="VER POOL DE ACTIVIDADES" /></Link>
+                  </div>
                </div>
             </div>
          </div>
       )
    }
 }
-
 
 
 
