5c101f2d82a005521045f4bd33d36f10f1e4203b
Lehara20 <74658801+Lehara20@users.noreply.github.com>
2022-12-03 14:48:46 +0100

    Update AddActividad.js

--- client/src/components/AddActividad.js
+++ client/src/components/AddActividad.js
@@ -1,226 +1,156 @@
-import axios from "axios"
-import React, { Component } from "react"
-// import ReactDOM from 'react-dom';
-import {Redirect, Link} from 'react-router-dom'
-// import { Link } from 'react-router-dom'
-// import { useState } from 'react';
-import Header from "./Header"
-import { SERVER_HOST } from "../config/global_constants"
-import "../css/AddActividad.css"
+/* #newDesrAct {
+    text-align: center;
+} */
 
-export default class AddActividad extends Component {
-   constructor(props) {
-      super(props);
+.inputLine,
+input,
+textarea {
+    width: 100%;
+    text-align: left;
+    margin-bottom: 5px;
+}
 
-      this.state = {
-         newNameAct: '',
-         newDesrAct: '',
-         enlaceVideo: '',
-         enlaceAudio: '',
-         image: [],
-         imagePath: [],
-         totalImages: 0,
-         redirectList: false,
-      };
-      this.listaImages = this.listaImages.bind(this);
-      this.handleChange = this.handleChange.bind(this);
-      this.handleSubmitData = this.handleSubmitData.bind(this);
-      this.handleSubmitImage = this.handleSubmitImage.bind(this);
-      this.deleteImage = this.deleteImage.bind(this);
-   }
+/* Formulario */
+.formulario {
+    min-width: 600px;
+    /* min-width: 1085px; */
+    width: 100%;
+    /* background-color: cadetblue; */
+    /* float:left;
+    width: 40%; */
+}
 
-   handleChange(event) {
-      this.setState({ [event.target.name]: event.target.value.trim() });
-   }
+/* Parte pictogramas */
+.secuenciaPicto {
+    width: 100%;
+    min-height: 300px;
+    /* background-color: plum; */
+    vertical-align: middle;
+    text-align: center;
+}
 
-   validate() {
-      if (this.state.newNameAct === '' || this.state.newDesrAct === '') {
-         alert(' Los campos de nombre y descripción no pueden estar vacíos');
-         return false;
-      }
-      return true;
-   }
+.scroll {
+    /* background-color: #c9deec; */
+    overflow-y: scroll;
+    max-height: 1000px;
+    border-color: #c9deec;
+    border-style: groove;
+    border-width: 1px;
+    border-radius: 20px;
+}
 
-   handleSubmitData(event) {
-      if (this.validate()) {
-         alert('Nombre: "' + this.state.newNameAct + '"\nDescripción: "' + this.state.newDesrAct + '"');
-         var bodyFormData = new FormData();
-         bodyFormData.append('nombre', this.state.newNameAct)
-         bodyFormData.append('descripcion', this.state.newDesrAct)
-         bodyFormData.append('enlaceVideo', this.state.enlaceVideo)
-         bodyFormData.append('enlaceAudio', this.state.enlaceAudio)
-         axios({
-            method: "post",
-            url: `${SERVER_HOST}/actividades/add`,
-            data: bodyFormData,
-            headers: { "Content-Type": "multipart/form-data" },
-         }).then(res => {
-            //handle success
-            alert('Actividad añadida, se van a guardar las imágenes...');
-            this.handleSubmitImage(event);
-         }).catch(err => {
-            //handle error
-            alert('No se ha podido guardar la actividad');
-            this.setState({ logInError: true, errorMessage: 'Error, no se ha podido guardar la actividad.' })
-         });
-         event.preventDefault();
-      }
-      // this.handleSubmitImage(event);
-   }
+.contImage {
+    /* width: 30px; */
+    width: 150px;
+    height: 150px;
+    vertical-align: middle;
+    justify-content: center;
+    text-align: center;
+    align-items: center;
+    background-color: white;
+    border-style: groove;
+    border-color: #4198d2;
+    border-width: 2px;
+    overflow: hidden;
+    display: flex;
 
-   // noImplementado
-   handleSubmitImage(event) {
-      let correcto = true;
-      for (let i = 0; i < this.state.totalImages; i++) {
-         // alert('Guardando imagen: "' + this.state.image[i].value + '"\nDescripción: "' + this.state.newDesrAct + '"');
-         var bodyFormData = new FormData();
-         bodyFormData.append('nombreAct', i + "::" + this.state.newNameAct)
-         bodyFormData.append('imagen', this.state.imagePath[i])
-         axios({
-            method: "post",
-            url: `${SERVER_HOST}/actividades/imgAdd`,
-            data: bodyFormData,
-            headers: { "Content-Type": "multipart/form-data" },
-         }).catch(err => {
-            //handle error
-            alert('No se ha podido guardar las imágenes');
-            correcto = false;
-            this.setState({ logInError: true, errorMessage: 'Error, no se ha podido guardar la actividad: ' + err })
-         });
-      }
-      if (correcto) {
-         alert("Imágenes guardadas correctamente")
-         this.setState({redirectList: true})
-      }
-   }
+}
 
-   handleFileChange = (event) => {
-      if (event.target.files) {
-         let reader = new FileReader();
-         reader.onload = (e) => {
-            // Metodo 1: No se  modifica directamente this.state pero slice es un método poco fiable (pueden mutar los datos)
-            let copyImage = this.state.image.slice();
-            copyImage.push(e.target.result);
-            // copyImagePath = this.state.imagePath.slice();
-            // copyImagePath.push(e.target.files);
-            this.setState({ image: copyImage }); //, imagePath : copyImagePath });
-            // Metodo 2: Se modifica directamente this.state pero los datos se modifican de manera más segura
-            // this.state.image.push(e.target.result);
-            // this.state.imagePath.push(e.target.files);
+.contenedorPicto {
+    vertical-align: middle;
+    justify-content: center;
+    text-align: center;
+    align-items: center;
 
-            this.setState({ totalImages: this.state.totalImages + 1 });
-         };
-         let copyImagePath = this.state.imagePath.slice();
-         copyImagePath.push(event.target.files[0].name);
-         reader.readAsDataURL(event.target.files[0]);
-         event.target.value = null;
-         this.setState({ imagePath: copyImagePath });
-      }
-   }
+    background-color: white;
+    border-style: groove;
+    border-color: #4198d2;
+    border-width: 2px;
+    overflow: hidden;
+    display: flex;
 
-   listaImages() {
-      const pictos = [];
-      for (let i = 0; i < this.state.totalImages; i++) {
-         pictos.push(
-            <div className="contenedorPicto objectLine" key="{i}" >
-               {i + 1}
-               <div className="contImage ">
-                  <img className="imagen" id="target" src={this.state.image[i]} />
-               </div>
-               <span>
-                  <button className="boton b1" onClick={() => { this.moveImage(i, (i - 1 + this.state.totalImages) % this.state.totalImages) }}>
-                     <span>&#8592;</span>
-                  </button>
-                  <button className="boton b1" onClick={() => { this.deleteImage(i) }}>
-                     <span>&#128465;</span>
-                  </button>
-                  <button className="boton b1" onClick={() => { this.moveImage(i, (i + 1) % this.state.totalImages) }}>
-                     <span>&#8594;</span>
-                  </button>
-               </span>
-            </div>
-         );
-      }
-      return pictos;
-   }
+}
 
-   moveImage(item, move) {
-      // alert('Se quiere mover la imagen ' + item + ' en la posición ' + move)
-      // Metodo 1: No se modifica directamente this.state pero slice es un método poco fiable (pueden mutar los datos)
-      let k, copyImage = this.state.image.slice();
-      k = copyImage[item];
-      copyImage[item] = copyImage[move]
-      copyImage[move] = k;
-      let copyImagePath = this.state.imagePath.slice();
-      k = copyImagePath[item];
-      copyImagePath[item] = copyImagePath[move]
-      copyImagePath[move] = k;
-      this.setState({ image: copyImage, imagePath: copyImagePath });
-      // Metodo 2: Se modifica directamente this.state pero los datos se modifican de manera más segura
-      // let k;
-      // k = this.state.image[item];
-      // this.state.image[item] = this.state.image[move]
-      // this.state.image[move] = k;
-      // k = this.state.imagePath[item];
-      // this.state.imagePath[item] = this.state.imagePath[move]
-      // this.state.imagePath[move] = k;
-      // this.setState({ totalImages: this.state.totalImages  });
-   }
+/* Botones */
+.parteBotones {
+    overflow: visible;
+    /* background-color: yellow; */
+    /* width: 8px; */
+    margin: 15px;
+    text-align: right;
+    /* float: right; */
+}
 
-   deleteImage(item) {
-      // alert('Se quiere eliminar la imagen ' + item)
-      // Metodo 1: No se modifica directamente this.state pero slice es un método poco fiable (pueden mutar los datos)
-      let copyImage = this.state.image.slice(), copyImagePath = this.state.imagePath.slice();
-      copyImage.splice(item, 1);
-      copyImagePath.splice(item, 1);
-      this.setState({ image: copyImage, imagePath: copyImagePath });
-      // this.setState({ totalImages: this.state.totalImages-1 });
-      // Metodo 2: Se modifica directamente this.state pero los datos se modifican de manera más segura
-      // this.state.image.splice(item, 1);
-      // this.state.imagePath.splice(item, 1);
+.botonAcciones {
+    font-size: 20px;
+    width: 200px;
+    height: 50px;
+    margin: 5px;
+    text-align: center;
 
-      this.setState({ totalImages: this.state.totalImages - 1 });
-   }
+}
 
-   render() {
-      return (
-         <div className="web-container">
-            <Header />
-            {this.state.redirectList ? <Redirect to="/ListaActividades"/> : null}
+.verde {
+    background-color: rgb(148, 240, 148);
+}
 
-            <h1>Añadir nueva actividad</h1>
-            <div className="formulario">
-               <form onSubmit={this.handleSubmitData}>
-                  <div>
-                     <label>Nombre de la actividad:</label><br />
-                     <input type="text" id="newNameAct" name="newNameAct" placeholder="Nombre de la nueva actividad" onChange={this.handleChange} />
-                  </div>
-                  <div>
-                     <label>Enlace a un vídeo:</label><br />
-                     <input type="text" id="enlaceVideo" name="enlaceVideo" placeholder="Enlace a un vídeo" onChange={this.handleChange} />
-                  </div>
-                  <div>
-                     <label>Enlace a un audio:</label><br />
-                     <input type="text" id="enlaceAudio" name="enlaceAudio" placeholder="Enlace a un audio" onChange={this.handleChange} />
-                  </div>
-                  <div>
-                     <label>Descripción:</label><br />
-                     <textarea id="newDesrAct" name="newDesrAct" placeholder="Descripción de la nueva actividad" onChange={this.handleChange} />
-                  </div>
-               </form>
-               <div>
-                  <h2>Secuencia de pictogramas</h2>
-                  <div id="ContenedorImagenes">
-                     {this.listaImages()}
-                  </div>
-                  <input type="file" onChange={this.handleFileChange} />
-               </div>
-               <button className="button" onClick={this.handleSubmitData}>
-                  Añadir Actividad
-               </button>
-            </div>
-            <Link to="/ListaActividades"><input id="toggleActividades" type="button" value="CANCELAR" /></Link>
-         </div>
-      );
-   }
+.rojo {
+    background-color: rgb(223, 124, 124);
+}
+
+/* Configuración de imagen genérica */
+.imagen {
+    width: 100%;
+    /* height: 100%; */
+    height: auto;
+    object-fit: contain;
+}
+
+/* Para que los objetos se alinéen en bloque en la misma línea */
+.objectline {
+    display: inline-block;
+    justify-content: space-between;
+    /* vertical-align: middle; */
+    vertical-align: top;
+
+}
+
+h2 {
+    color: #4198d2;
+    font-size: 30px;
+    text-align: center;
+}
+
+.web-container {
+    /* text-align: center; */
+    vertical-align: text-top;
+}
+
+@media screen and (min-width: 1085px) {
+
+    /* Formulario */
+    .formulario {
+        min-width: 600px;
+        width: 30%;
+        /* background-color: blueviolet; */
+        /* float:left;
+    width: 40%; */
+    }
+
+    .secuenciaPicto {
+        width: 55%;
+        /* background-color: plum; */
+        vertical-align: middle;
+        text-align: center;
+    }
+
+    .both {
+        display: flex;
+        /* equal height of the children */
+    }
+
+    .parte {
+        flex: 1;
+        /* additionally, equal width */
+    }
 }
